24477fb84608923027d0f3fa7b181cc1efe61905
niazyeladawy <niazyadawy2@gmail.com>
2022-06-27 15:14:25 +0200

    participannt edit

--- src/components/manage competitions/participants/Participants.js
+++ src/components/manage competitions/participants/Participants.js
@@ -64,136 +64,148 @@ const Participants = () => {
                             <thead className='bg-main'>
                                 <tr>
                                     <td></td>
-                                    <td>School/Tuition</td>
+                                    <td>Name</td>
                                     <td>Country</td>
-                                    <td>Email</td>
-                                    <td>Teachers</td>
-                                    <td>Phone Number   </td>
-                                    <td>Address</td>
-                                    <td>Postal Code</td>
-                                    <td>Province/State</td>
+                                    <td>Index</td>
+                                    <td>Password</td>
+                                    <td>Grade</td>
+                                    <td>School</td>
+                                    <td>Class</td>
+                                    <td>Tuition Centre</td>
+                                    <td>Private</td>
+                                    <td>Competition/Assement</td>
+                                    <td>Partner </td>
+                                    <td>Teachers </td>
                                     <td>Created By</td>
-                                    <td>Approved By</td>
-                                    <td>Rejected By</td>
                                     <td>Last Modified By</td>
                                     <td>Status</td>
-
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
                                     <td>1</td>
                                     <td className='approved_icons'>Lorem, ipsum.
-                                        <div className='school__icons'>
+                                        <div className='participant__icons'>
                                             <i className="fa-solid fa-pen-to-square me-2"></i>
                                             <i className="fa-solid fa-trash me-2"></i>
-                                            <i class="fa-solid fa-user-plus"></i>
-                                            <i class="fa-solid fa-user-ninja"></i>
+                                            <i class="fa-solid fa-copy  me-2"></i>
+                                            <i class="fa-solid fa-pen me-2fa-solid fa-pen"></i>
                                         </div>
                                     </td>
                                     <td>Lorem.</td>
-                                    <td>Lorem, ipsum.@email.com</td>
-                                    <td>5   </td>
-                                    <td>0103166314</td>
+                                    <td>541841</td>
+                                    <td>adasd   </td>
+                                    <td>1</td>
                                     <td>Lorem ipsum dolor sit.</td>
                                     <td>Lorem, ipsum.</td>
                                     <td>Lorem, ipsum.</td>
                                     <td>Lorem, ipsum.</td>
                                     <td>Lorem, ipsum.</td>
                                     <td>Lorem, ipsum.</td>
+                                    <td className=''>kelly</td>
                                     <td className=''>kelly(2020/05/5 13:00)</td>
-                                    <td className=''><span className='status-btn approved'>Approved</span></td>
+                                    <td className=''>kelly(2020/05/5 13:00)</td>
+                                    <td className=''><span className='status-btn active-btn'>Active</span></td>
                                 </tr>
                                 <tr>
-                                    <td>2</td>
+                                    <td>1</td>
                                     <td className='approved_icons'>Lorem, ipsum.
-                                        <div className='school__icons'>
+                                        <div className='participant__icons'>
                                             <i className="fa-solid fa-pen-to-square me-2"></i>
                                             <i className="fa-solid fa-trash me-2"></i>
-                                            <i class="fa-solid fa-user-plus me-2"></i>
-                                            <i class="fa-solid fa-user-ninja me-2"></i>
+                                            <i class="fa-solid fa-copy  me-2"></i>
+                                            <i class="fa-solid fa-pen me-2fa-solid fa-pen"></i>
                                         </div>
                                     </td>
                                     <td>Lorem.</td>
-                                    <td>Lorem, ipsum.@email.com</td>
-                                    <td>5   </td>
-                                    <td>0103166314</td>
+                                    <td>541841</td>
+                                    <td>adasd   </td>
+                                    <td>1</td>
                                     <td>Lorem ipsum dolor sit.</td>
                                     <td>Lorem, ipsum.</td>
                                     <td>Lorem, ipsum.</td>
                                     <td>Lorem, ipsum.</td>
                                     <td>Lorem, ipsum.</td>
                                     <td>Lorem, ipsum.</td>
+                                    <td className=''>kelly</td>
                                     <td className=''>kelly(2020/05/5 13:00)</td>
-                                    <td className=''><span className='status-btn approved'>Approved</span></td>
+                                    <td className=''>kelly(2020/05/5 13:00)</td>
+                                    <td className=''><span className='status-btn active-btn'>Active</span></td>
                                 </tr>
                                 <tr>
-                                    <td>3</td>
-                                    <td className='pending_icons'>Lorem, ipsum.
-                                        <div className='school__icons'>
+                                    <td>1</td>
+                                    <td className='approved_icons'>Lorem, ipsum.
+                                        <div className='participant__icons'>
                                             <i className="fa-solid fa-pen-to-square me-2"></i>
                                             <i className="fa-solid fa-trash me-2"></i>
-                                            <i class="fa-solid fa-square-check me-2"></i>
-                                            <i class="fa-solid fa-square-xmark me-2"></i>
+                                            <i class="fa-solid fa-copy  me-2"></i>
+                                            <i class="fa-solid fa-pen me-2fa-solid fa-pen"></i>
                                         </div>
                                     </td>
                                     <td>Lorem.</td>
-                                    <td>Lorem, ipsum.@email.com</td>
-                                    <td>5   </td>
-                                    <td>0103166314</td>
+                                    <td>541841</td>
+                                    <td>adasd   </td>
+                                    <td>1</td>
                                     <td>Lorem ipsum dolor sit.</td>
                                     <td>Lorem, ipsum.</td>
                                     <td>Lorem, ipsum.</td>
                                     <td>Lorem, ipsum.</td>
                                     <td>Lorem, ipsum.</td>
                                     <td>Lorem, ipsum.</td>
+                                    <td className=''>kelly</td>
                                     <td className=''>kelly(2020/05/5 13:00)</td>
-                                    <td className=''><span className='status-btn pending'>Approved</span></td>
+                                    <td className=''>kelly(2020/05/5 13:00)</td>
+                                    <td className=''><span className='status-btn absent'>Absent</span></td>
                                 </tr>
                                 <tr>
-                                    <td>4</td>
-                                    <td className='pending_icons'>Lorem, ipsum.
-                                        <div className='school__icons'>
+                                    <td>1</td>
+                                    <td className='approved_icons'>Lorem, ipsum.
+                                        <div className='participant__icons'>
                                             <i className="fa-solid fa-pen-to-square me-2"></i>
                                             <i className="fa-solid fa-trash me-2"></i>
-                                            <i class="fa-solid fa-square-check me-2"></i>
-                                            <i class="fa-solid fa-square-xmark me-2"></i>
+                                            <i class="fa-solid fa-copy  me-2"></i>
+                                            <i class="fa-solid fa-pen me-2fa-solid fa-pen"></i>
                                         </div>
                                     </td>
                                     <td>Lorem.</td>
-                                    <td>Lorem, ipsum.@email.com</td>
-                                    <td>5   </td>
-                                    <td>0103166314</td>
+                                    <td>541841</td>
+                                    <td>adasd   </td>
+                                    <td>1</td>
                                     <td>Lorem ipsum dolor sit.</td>
                                     <td>Lorem, ipsum.</td>
                                     <td>Lorem, ipsum.</td>
                                     <td>Lorem, ipsum.</td>
                                     <td>Lorem, ipsum.</td>
                                     <td>Lorem, ipsum.</td>
+                                    <td className=''>kelly</td>
                                     <td className=''>kelly(2020/05/5 13:00)</td>
-                                    <td className=''><span className='status-btn pending'>Approved</span></td>
+                                    <td className=''>kelly(2020/05/5 13:00)</td>
+                                    <td className=''><span className='status-btn computed'>Computed</span></td>
                                 </tr>
                                 <tr>
-                                    <td>5</td>
-                                    <td className='rejected_icons'>Lorem, ipsum.
-                                        <div className='school__icons'>
+                                    <td>1</td>
+                                    <td className='approved_icons'>Lorem, ipsum.
+                                        <div className='participant__icons'>
                                             <i className="fa-solid fa-pen-to-square me-2"></i>
-                                            <i class="fa-solid fa-trash-arrow-up me-2"></i>
-
+                                            <i className="fa-solid fa-trash me-2"></i>
+                                            <i class="fa-solid fa-copy  me-2"></i>
+                                            <i class="fa-solid fa-pen me-2fa-solid fa-pen"></i>
                                         </div>
                                     </td>
                                     <td>Lorem.</td>
-                                    <td>Lorem, ipsum.@email.com</td>
-                                    <td>5   </td>
-                                    <td>0103166314</td>
+                                    <td>541841</td>
+                                    <td>adasd   </td>
+                                    <td>1</td>
                                     <td>Lorem ipsum dolor sit.</td>
                                     <td>Lorem, ipsum.</td>
                                     <td>Lorem, ipsum.</td>
                                     <td>Lorem, ipsum.</td>
                                     <td>Lorem, ipsum.</td>
                                     <td>Lorem, ipsum.</td>
+                                    <td className=''>kelly</td>
                                     <td className=''>kelly(2020/05/5 13:00)</td>
-                                    <td className=''><span className='status-btn rejected'>Deleted</span></td>
+                                    <td className=''>kelly(2020/05/5 13:00)</td>
+                                    <td className=''><span className='status-btn computed'>Computed</span></td>
                                 </tr>
                             </tbody>
                         </table>
